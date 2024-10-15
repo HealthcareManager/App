d434bbefd63815949863df6b2892184ce18920b0
PaulKo121 <kyc881201@gmail.com>
2024-10-15 12:01:46 +0800

    LINE PAY付款邏輯 修改傳遞數據

--- app/src/main/java/com/luce/healthmanager/ParseTokenTask.java
+++ app/src/main/java/com/luce/healthmanager/ParseTokenTask.java
@@ -27,8 +27,8 @@ public class ParseTokenTask extends AsyncTask<String, Void, JSONObject> {
         JSONObject userData = null;
 
         try {
-            URL url = new URL("https://healthcaremanager.myvnc.com:8443/HealthcareManager/api/auth/validate-token");
-            //URL url = new URL("http://10.0.2.2:8080/api/auth/validate-token");
+//            URL url = new URL("https://healthcaremanager.myvnc.com:8443/HealthcareManager/api/auth/validate-token");
+            URL url = new URL("http://10.0.2.2:8080/api/auth/validate-token");
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("POST");
             connection.setRequestProperty("Authorization", "Bearer " + token);
